46acb48be21ad4ce0544de5c510f281e67585c67
Dahveyea <dahveyea.cowan@gmail.com>
2019-12-10 15:21:51 -0800

    Added databases and made new routes

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
+from bson import ObjectId
 from flask import Flask, render_template, redirect, url_for, request
+from pymongo import MongoClient
+import os
+
 
 app = Flask(__name__)
 
+
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/AnimeArtStore')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+
+
+anime = db.anime
+
+
 @app.route('/')
 def index():
-    """Return homepage."""
-    return render_template('index.html')
+    """Homepage for what you want to search."""
+    return render_template('index.html', anime=anime.find())
 
 
-@app.route('/anime')
-def anime():
+@app.route('/anime/<anime_id>')
+def anime_show(anime_id):
     """Return homepage."""
-    return render_template('anime.html')
+    AnimeArt = anime.find_one({'_id': ObjectId(AnimeArt_id)})
+    return render_template('anime_show.html', anime=anime)
+
+
+@app.route('/anime/new')
+def anime_new():
+    '''This is for a listing'''
+    return render_template('anime_new.html',anime={}, title='Add a Anime Art')
+
 
 
 @app.route('/AboutAnime')
