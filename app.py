3deb4cbecb749acfb6b472fc47418521c6e4f479
Dahveyea <dahveyea.cowan@gmail.com>
2019-12-11 15:23:40 -0800

    Added delete route function and fixed Interal server error for my show route

--- app.py
+++ app.py
@@ -21,28 +21,26 @@ def animes_ndex():
     """Homepage for what you want to search."""
     return render_template('animes_index.html', animes=animes.find())
 
-
-@app.route('/anime')
-def anime():
-    """Return homepage."""
-    return render_template('animes_new.html')
+@app.route('/newAnime')
+def newAnime():
+    return render_template('animes_show.html')
 
 
 @app.route('/animes/<anime_id>')
 def animes_show(anime_id):
-    """Return homepage."""
+    """Show the info for each Anime Art."""
     anime = animes.find_one({'_id': ObjectId(anime_id)})
     return render_template('animes_show.html', anime=anime)
 
 
 @app.route('/animes/new')
 def animes_new():
-    '''This is for a listing'''
+    '''This is for a Anime Art listing'''
     return render_template('animes_new.html',anime={}, title='Add a Anime Art')
 
 @app.route('/animes', methods=['POST'])
 def animes_submit():
-    ''' add new anime to the database and redirect to that anime's page '''
+    ''' add new anime art to the database and redirect to that anime's page '''
     anime = {
         'name': request.form.get('name'),
         'description': request.form.get('description'),
@@ -54,13 +52,13 @@ def animes_submit():
 
 @app.route('/animes/<anime_id>/edit')
 def anime_edit(anime_id):
-    ''' form to edit a plant's listing '''
-    anime = animes.find_one({'_id': ObjectId(plant_id)})
+    ''' form to edit a Anime Art listing '''
+    anime = animes.find_one({'_id': ObjectId(anime_id)})
     return render_template('animes_edit.html', anime=anime, title='Edit Listing')
 
 @app.route('/animes/<anime_id>', methods=['POST'])
 def animes_update(anime_id):
-    ''' add updated info of an anime to the database and redirect to that animes page '''
+    ''' add updated info of an anime art to the database and redirect to that anime art's  page '''
     updated_anime = {
         'name': request.form.get('name'),
         'description': request.form.get('description'),
@@ -74,7 +72,11 @@ def animes_update(anime_id):
 
 
 
-
+@app.route('/anime/<anime_id>/delete', methods=['POST'])
+def animes_delete(anime_id):
+    ''' delete a anime art from the database, redirect to the home page '''
+    animes.delete_one({'_id': ObjectId(anime_id)})
+    return redirect(url_for('animes_index'))
 
 @app.route('/AboutAnime')
 def AboutAnime():
